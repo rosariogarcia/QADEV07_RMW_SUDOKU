4326ad3f31706d236a6f0c29359b93b28467996a
RosarioGarcia <RosarioGarcia@QDV07DRG1001.fundacion-jala.local>
2016-03-21 14:31:52 -0400

    to test egit

---
+++ Sudoku/src/game/sudoku/DifficultLevel.java
@@ -0,0 +1,17 @@
+package game.sudoku;
+
+public class DifficultLevel {
+	private String name ;
+	private int topLimit;
+	private int bootomLimit;
+	
+	public DifficultLevel(String name, int topLimit, int bootomLimit)
+	{
+		this.name = name;
+		this.topLimit = topLimit;
+		this.bootomLimit = bootomLimit;
+	}
+	
+	
+	
+}
