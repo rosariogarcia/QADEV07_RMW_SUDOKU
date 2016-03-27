233b352d74d13d76c05715bda13c0dcf0f97605f
WilmaPaca <Wilma.Paca@fundacion-jala.org>
2016-03-27 09:26:01 -0400

    Update FileTxt.java

--- Sudoku/src/game/sudoku/FileTxt.java
+++ Sudoku/src/game/sudoku/FileTxt.java
@@ -4,6 +4,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.StringTokenizer;
+import java.io.FileReader;
 
 public class FileTxt implements File {
 
@@ -15,6 +19,11 @@ public class FileTxt implements File {
 	final static String NEW_LINE_SEPARATOR = "\n";
 	final static String character = "";
 	
+	     /**
+	     * @param array
+	     * @throws IOException 
+	     * Read a array and write on a file
+	     */
 	@Override
 	public void writeFileArray(String[][] array) throws IOException {
 		// TODO Auto-generated method stub
@@ -42,10 +51,34 @@ public class FileTxt implements File {
 		}
 		
 	}
-
+	     /**
+	     * @param file
+	     * @throws IOException 
+	     * Read a file TXT and put on a array
+	     */
 	@Override
 	public void readFileArray(String file) throws IOException {
 		// TODO Auto-generated method stub
+		String[][] data = new String[9][9];
+		int row = 0;
+		int col = 0;
+		try {
+			FileReader fileReader = new FileReader(file);
+			BufferedReader stdin = new BufferedReader(fileReader);
+			
+			String line = null;
+			while ((line = reader.readLine()) != null && row < data.length) {
+			StringTokenizer st = new StringTokenizer(line, "\t");
+				while (st.hasMoreTokens()) {
+				data[col][row] = String.valueOf(st.nextToken());
+				
+				col++;
+
+				}
+
+			col = 0;
+			row++;
+			}
 
 	}
 
