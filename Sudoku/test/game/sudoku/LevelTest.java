dfefb83086188aa9405022a390f222a64fb32763
rosariogarcia <Rosario.Garcia@fundacion-jala.org>
2016-03-27 20:25:08 -0400

    Update LevelTest

--- Sudoku/test/game/sudoku/LevelTest.java
+++ Sudoku/test/game/sudoku/LevelTest.java
@@ -1,24 +1,42 @@
-package game.sudoku;
+package game.sudouku;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
 
 public class LevelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void levelCanBeCreatedWithNameTopLimitAndBottomLimitParameters(){
+		Level newLevel = new Level("Easy",75,70);
+		assertTrue(newLevel instanceof Level);
 	}
 	
 	@Test
-	public void isOutOfLimits(int limit) {
-        return blankCells.size() >= limit;
-    }
-
-
-
-    public int getBlankCellsNumber(){
-        return topLimit-bottomLimit;
-    }
+	public void levelCreatedWithNameTopLimitAndBottomLimitParametersShouldReturnThem(){
+		Level anotherLevel = new Level("Easy",75,70);
+		assertEquals("Easy",anotherLevel.getName());
+		assertEquals(75,anotherLevel.getTopLimit());
+		assertEquals(70,anotherLevel.getBottomLimit());
+	}
+	
+	@Test
+	public void topLimitAndBottomLimitShouldMajorThanZero(){
+		Level anotherLevel = new Level("Easy",75,70);
+		assertTrue(anotherLevel.getTopLimit()>0);
+		assertTrue(anotherLevel.getBottomLimit()>0);
+	}
+	
+	@Test
+	public void topLimitAndBottomLimitShouldBeDiferents(){
+		Level anotherLevel = new Level("Easy",75,70);
+		assertFalse(anotherLevel.getTopLimit()==anotherLevel.getBottomLimit());
+	}
+	
+	@Test
+	public void theRandomValueShouldBeBetweentopLimitAndBottomLimit(){
+		Level anotherLevel = new Level("Easy",75,70);
+		assertTrue(	anotherLevel.getBlankCellsNumber()	>	anotherLevel.getBottomLimit() && 
+					anotherLevel.getBlankCellsNumber()	<= 	anotherLevel.getTopLimit());
+	}	
 }
