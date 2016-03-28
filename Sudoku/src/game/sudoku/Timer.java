f64d3167e1e7a71421201e2bb76512fdf0cc18c4
rosariogarcia <Rosario.Garcia@fundacion-jala.org>
2016-03-27 20:29:06 -0400

    Update Timer.java

--- Sudoku/src/game/sudoku/Timer.java
+++ Sudoku/src/game/sudoku/Timer.java
@@ -1,5 +1,20 @@
-package game.sudoku;
+package game.sudouku;
 
 public class Timer {
+	
+	long timeInitial, timeFinal;
+	
+	public Timer(){
+	   this.timeInitial = System.currentTimeMillis();
+	}
+
+	public long getTimeInitial() {
+		return timeInitial;
+	}
+
+	public long timeSolve(){
+	   timeFinal = System.currentTimeMillis() - timeInitial;
+	   return timeFinal;
+	}
 
 }
