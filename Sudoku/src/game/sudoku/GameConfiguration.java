404e57002e20d340002a74b97c168c17085d0bc3
MijhailVillarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-03-28 16:13:26 -0400

    Update GameConfiguration.java

--- Sudoku/src/game/sudoku/GameConfiguration.java
+++ Sudoku/src/game/sudoku/GameConfiguration.java
@@ -1,5 +1,5 @@
+package game.sudouku;
 import java.util.List;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
