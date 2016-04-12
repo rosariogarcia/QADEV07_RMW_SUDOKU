46ef026e72bc2ef591f82cc91d3c0a036ddeb683
WilmaPaca <Wilma.Paca@fundacion-jala.org>
2016-04-12 14:34:50 -0400

    Update FileCsv.java

--- Sudoku/src/game/sudoku/FileCsv.java
+++ Sudoku/src/game/sudoku/FileCsv.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class FileCsv implements File {
+public class FileCsv extends File {
 	int row = 0;
 	int col = 0;
 
@@ -16,10 +16,8 @@ public class FileCsv implements File {
 	 * This method write in a file csv the value of a array that contain string
 	 * data and save it in a place specific from the PC
 	 * 
-	 * @param arraySudoku
-	 *            the arraySudoku contain the data to export to the file
-	 * @param pathFile
-	 *            the pathFile contain the root and the name file
+	 * @param arraySudoku  	the arraySudoku contain the data to export to the file
+	 * @param pathFile	the pathFile contain the root and the name file
 	 */
 
 	@Override
@@ -39,8 +37,7 @@ public class FileCsv implements File {
 	 * This method read from the file and put the data in a array of integer
 	 * value
 	 * 
-	 * @param file
-	 *            the file to allow to read the data from the file
+	 * @param file	the file to allow to read the data from the file
 	 */
 	@Override
 	public void readFileArray(String file) {
@@ -60,12 +57,9 @@ public class FileCsv implements File {
 	 * This method run every line of the file in put in a specific place in a
 	 * array
 	 * 
-	 * @throws IOException
-	 *             If an input or output exception occurred
-	 * @param dataSudoku
-	 *            the dataSudoku to display a array of string of 9X9
-	 * @param stdin
-	 *            the stdin to allow to read of the file txt
+	 * @throws IOException	If an input or output exception occurred
+	 * @param dataSudoku	the dataSudoku to display a array of string of 9X9
+	 * @param stdin		the stdin to allow to read of the file txt
 	 */
 	private void putOnArraySudoku(int[][] dataSudoku, BufferedReader stdin)
 			throws IOException {
@@ -85,12 +79,9 @@ public class FileCsv implements File {
 	 * This method runs every segment of the array to put in a segment from the
 	 * file
 	 * 
-	 * @throws IOException
-	 *             If an input or output exception occurred
-	 * @param arraySudoku
-	 *            the arraySudoku to contain a array of string of 9X9
-	 * @param fileWriter
-	 *            the fileWriter to allow to write in the file txt
+	 * @throws IOException	If an input or output exception occurred
+	 * @param arraySudoku	the arraySudoku to contain a array of string of 9X9
+	 * @param fileWriter	the fileWriter to allow to write in the file txt
 	 */
 	private void getContentFile(String[][] arraySudoku, FileWriter fileWriter)
 			throws IOException {
