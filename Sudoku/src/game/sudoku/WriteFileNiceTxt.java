e386ddbee2947bb3ecc2d0d58eeb7590fda38285
WilmaPaca <Wilma.Paca@fundacion-jala.org>
2016-04-11 20:13:15 -0400

    Update commit
    Signed-off-by: WilmaPaca <Wilma.Paca@fundacion-jala.org>

--- Sudoku/src/game/sudoku/WriteFileNiceTxt.java
+++ Sudoku/src/game/sudoku/WriteFileNiceTxt.java
@@ -8,14 +8,13 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * @author WilmaPaca
- * 
- */
 public class WriteFileNiceTxt implements File {
 	/**
-	 * @param arraySudoku
-	 * @param pathFile
+	 * This method write in a file txt the value of a array that contain string
+	 * data and save it in a place specific from the PC
+	 * 
+	 * @param arraySudoku	the arraySudoku contain the data to export to the file
+	 * @param pathFile		the pathFile contain the root and the name file
 	 */
 	@Override
 	public void writeFileArray(String[][] arraySudoku, String pathFile) {
@@ -31,10 +30,12 @@ public class WriteFileNiceTxt implements File {
 	}
 
 	/**
+	 * This method runs every segment of the array to put in a segment from the
+	 * file
 	 * 
-	 * @param arraySudoku
-	 * @param fileWriter
-	 * @throws IOException
+	 * @param arraySudoku	contain the data for put in a file 
+	 * @param fileWriter	the fileWriter to allow to write in the file txt
+	 * @throws IOException	If an input or output exception occurred internally in the class
 	 */
 	private void getContentFile(String[][] arraySudoku, FileWriter fileWriter)
 			throws IOException {
@@ -50,10 +51,12 @@ public class WriteFileNiceTxt implements File {
 	}
 
 	/**
+	 * This method put "--" and "+" in the output of the file with format on every 
+	 * third and sixth row
 	 * 
-	 * @param fileWriter
-	 * @param row
-	 * @throws IOException
+	 * @param fileWriter	the fileWriter to allow to write in the file txt with format
+	 * @param row			is the row what it will have a print "-" and "+"
+	 * @throws IOException	If an input or output exception occurred internally in the class
 	 */
 	private void putLimitRowBox(FileWriter fileWriter, int row)
 			throws IOException {
@@ -64,11 +67,11 @@ public class WriteFileNiceTxt implements File {
 
 	/**
 	 * 
-	 * @param arraySudoku
-	 * @param row
-	 * @param content
-	 * @param col
-	 * @return
+	 * @param arraySudoku	contain the data for put in a file
+	 * @param row			is the row what it will have a print "|" 
+	 * @param content		contain all row of the arraySudoku 
+	 * @param col			is the column what it will have a print "|"
+	 * @return				return the value of the column and row
 	 */
 	private String putLimitColumnBox(String[][] arraySudoku, int row,
 			String content, int col) {
