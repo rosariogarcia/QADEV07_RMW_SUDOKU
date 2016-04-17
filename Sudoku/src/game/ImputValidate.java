982c5c7a26472ca1a15dd7e08ea4aa64bc5c9ae4
MijhailVillarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-04-17 11:46:38 -0400

    Fix

--- Sudoku/src/game/ImputValidate.java
+++ Sudoku/src/game/ImputValidate.java
@@ -6,50 +6,57 @@ public class ImputValidate {
     
     /**
      * converts a String to a two-dimensional array
-     * @param stringReed for convert a array
+     * @param stringRead for convert a array
      * @return A two-dimensional array
      */
 
-	public static int[][] convertToMatrix(String stringReed) {
-		int[][] grid = new int[9][9];
-		int row = 0;
-		int col = 0;
-		int sizeGrid = 8;
-		for (char number : stringReed.toCharArray()) {
-			grid[row][col] = Integer.parseInt(number + "");
-			if (col++ == sizeGrid) {
-				row++;
-				col = 0;
-				sizeGrid = +8;
-			}
-		}
-		return grid;
-	}
+    public static int[][] convertToMatrix(String stringRead) {
+        int[][] grid = new int[9][9];
+        int row = 0;
+        int col = 0;
+        int sizeGrid = 8;
+        for (char number : stringRead.toCharArray()) {
+            grid[row][col] = Integer.parseInt(number + "");
+            if (col++ == sizeGrid) {
+                row++;
+                col = 0;
+                sizeGrid = +8;
+            }
+        }
+        return grid;
+    }
 
-	/**
-	 * Verified if String is a number
-	 * @param stringReed for convert a array
-	 * @return return true if convert the String to a number
-	 */
-	public static boolean isNumeric(String stringReed) {
-		try {
-			for (char number : stringReed.toCharArray()) {
-				Integer.parseInt(number + "");
-			}
+    /**
+     * Verified if String is a number
+     * @param stringRead for convert a array
+     * @return return true if convert the String to a number
+     */
+    public static boolean isNumeric(String stringRead) {
+        try {
+           
+                Integer.parseInt(stringRead );
+            
 
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
-	/**
-	 *  Verified that contain 81 characters. 
-	 * @param stringReed for convert a array
-	 * @return true if contain 81 position.
-	 */
-	public static boolean contain81position(String stringReed) {
-		return stringReed.length() == 81;
-	}
+    /**
+     *  Verified that contain 81 characters. 
+     * @param stringRead for convert a array
+     * @return true if contain 81 position.
+     */
+    public static boolean contain81position(String stringRead) {
+        return stringRead.length() == 81;
+    }
+    
+    public static boolean number1_9(int numberRead) {
+       if(numberRead >= 0 && numberRead<=9)
+           return true;
+       return false;
+    }
 
 }
+
--- Sudoku/src/game/Reedkeyboard.java
+++ Sudoku/src/game/Reedkeyboard.java
@@ -3,22 +3,26 @@ package game;
 import java.util.Scanner;
 
 public class Reedkeyboard {
-
-	public static int[][] reed() {
-		Scanner scanner = new Scanner(System.in);
-		boolean endMenu = false;
-		int[][] result = null;
-		do {
-			System.out.println("Enter a sudoku");
-			String stringReed = scanner.next();
-			if (stringReed.length() == 81 && ImputValidate.isNumeric(stringReed)) {
-				endMenu = true;
-				result = ImputValidate.convertToMatrix(stringReed);
-			}
-
-		} while (endMenu == false);
-
-		return result;
-	}
-
+    
+    public static int [][] reed(){
+        Scanner scanner = new Scanner(System.in);
+        boolean endMenu = false;
+        int [][] result = null;
+        do{
+            System.out.println("Enter a sudoku");
+            String stringReed = scanner.next();
+            if ( stringReed.length()== 81 && ImputValidate.isNumeric(stringReed))
+            {
+                endMenu = true;
+                result = ImputValidate.convertToMatrix(stringReed);
+            }
+          
+        }while(endMenu == false);
+  
+        return result;
+    }
+    
+    
+    
+   
 }
